627452dbbf5e3ab615c64b70c2e782e0eebfa2d1
Rudolf Grauberger <rudolf@grauberger.org>
2019-01-17 18:11:50 +0100

    Adds error messages...

--- src/main/java/client/localization/Main.java
+++ src/main/java/client/localization/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -220,11 +221,9 @@ public class Main extends Application  implements IMoveController{
             connected = true;
 
         } catch (Exception e) {
-            //console.log(e.getMessage());
-            //error(e.getMessage());
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
         }
 
-        //info(String.format("Conntection established to %s on Port %d...", host, port));
         switchConnectedButton();
     }
 
@@ -233,7 +232,7 @@ public class Main extends Application  implements IMoveController{
             myclient.close();
             connected = false;
         } catch (IOException e) {
-            //error(e.getMessage());
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
         }
 
         switchConnectedButton();

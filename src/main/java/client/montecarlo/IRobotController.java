919605215bd3dfa280f2827d1140bdff0bc5f059
Marvin Biesenbach <marvin.biesenbach@smail.th-koeln.de>
2019-01-17 16:55:42 +0100

    delete :*

--- src/main/java/client/montecarlo/IRobotController.java
+++
@@ -1,9 +0,0 @@
-package client.montecarlo;
-
-public interface IRobotController {
-    SensorDataSet getSensorDataSet() throws ActionException;
-    void moveForward ( int cm )throws ActionException;
-    void moveBackward ( int cm)throws ActionException;
-    void turnLeft ( int angle )throws ActionException;
-    void turnRight ( int angle )throws ActionException;
-}
